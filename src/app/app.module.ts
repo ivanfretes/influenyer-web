4006b49050e372e4ef81c3b0d35095b2f4653330
Cristian Ortega <orte.ca1208@gmail.com>
2020-07-25 12:17:56 -0400

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './components/compartido/menu/menu.component';
+//import { MenuComponent } from './components/compartido/menu/menu.component';
 //mport { ContactosComponent } from './components/compartido/contactos/contactos.component';
 //import { ProductosComponent } from './components/compartido/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
+    //MenuComponent,
     //ContactosComponent,
     //ProductosComponent, 
     routingComponent
