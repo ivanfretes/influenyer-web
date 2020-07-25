e77a037069e48b65167685c07a72a178a38912c4
Cristian Ortega <orte.ca1208@gmail.com>
2020-07-25 14:04:35 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 //import { MenuComponent } from './components/compartido/menu/menu.component';
-//mport { ContactosComponent } from './components/compartido/contactos/contactos.component';
+//import { ContactosComponent } from './components/compartido/contactos/contactos.component';
 //import { ProductosComponent } from './components/compartido/productos/productos.component';
 
 @NgModule({
