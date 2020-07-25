09054afc38e6d8752a925044b4d686bc56ff5c6f
Cristian Ortega <orte.ca1208@gmail.com>
2020-07-25 14:27:49 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 //import { ProductosComponent } from './components/compartido/productos/productos.component';
 
 @NgModule({
-  declarations: [
+  declarations: [  
     AppComponent,
     //MenuComponent,
     //ContactosComponent,
